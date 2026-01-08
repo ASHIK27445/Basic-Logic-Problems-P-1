5e2e8164f8488afc27f01649fdadcdc1e896f1cd
ASHIK27445 <mdashikulislama57@gmail.com>
2026-01-08 18:40:50 +0600

    Solved the Problem 4

--- problem3.js
+++ problem3.js
@@ -1,8 +1,6 @@
 const palindrome = (str) => {
     const original = str.toLowerCase()
-    console.log(original)
     const reverse = str.split('').reverse().join('')
-    console.log(reverse)
     if(original === reverse){
         return true
     }else{
---
+++ problem4.js
@@ -0,0 +1,13 @@
+const findMaxNum = (num) => {
+    let max = num[0]
+
+    for(let i=0 ; i<num.length; i++){
+        if(num[i] > max){
+            max = num[i]
+        }
+    }
+
+    return max
+}
+
+console.log(findMaxNum([5, 1, 9, 3]))
