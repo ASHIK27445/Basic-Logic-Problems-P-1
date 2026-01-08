868a0752817637bd1fd95d7a243ca38c5161933c
ASHIK27445 <mdashikulislama57@gmail.com>
2026-01-08 18:49:38 +0600

    Solved Problem 6

---
+++ problem6.js
@@ -0,0 +1,11 @@
+const sumAllNum = (numbers)=>{
+    let sum = 0
+
+    for(let i=0; i<numbers.length; i++){
+        sum += numbers[i]
+    }
+
+    return sum
+}
+
+console.log(sumAllNum([1, 2, 3, 4]))
